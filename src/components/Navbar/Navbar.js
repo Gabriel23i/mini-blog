dbfbe2701c9969db61b8e645bf797d6f7cd05737
Gabriel23i <ogabrielmb@gmail.com>
2022-10-12 14:43:23 -0300

    autentication and routes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { useAuthentication } from '../../hooks/useAuthentication';
+import { useAuthValue } from '../../context/AuthContext';
+
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+    const { user } = useAuthValue();
   return (
     <nav className={styles.navbar}>
         <NavLink className={styles.brand} to='/'>
@@ -19,22 +23,46 @@ const Navbar = () => {
                     Home
                 </NavLink>
             </li>
-            <li>
-                <NavLink
-                    className={({ isActive }) => (isActive ? styles.active : '')}
-                    to='/login'
-                >
-                    Entrar
-                </NavLink>
-            </li>
-            <li>
-                <NavLink
-                    className={({ isActive }) => (isActive ? styles.active : '')}
-                    to='/register'
-                >
-                    Cadastrar
-                </NavLink>
-            </li>
+            {!user && (
+                <>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? styles.active : '')}
+                            to='/login'
+                        >
+                            Entrar
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? styles.active : '')}
+                            to='/register'
+                        >
+                            Cadastrar
+                        </NavLink>
+                    </li>
+                </>
+            )}
+            {user && (
+                <>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? styles.active : '')}
+                            to='/posts/create'
+                        >
+                            Novo post
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? styles.active : '')}
+                            to='/dashboard'
+                        >
+                            Dashboard
+                        </NavLink>
+                    </li>
+                </>
+            )}
             <li>
                 <NavLink
                     className={({ isActive }) => (isActive ? styles.active : '')}
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom';
 import styles from './About.module.css';
 
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um Blog feito com React no Front-end e Firebase no Back-end.
+      </p>
+      <Link className='btn' to='/posts/create'>
+        Criar post
+      </Link>
     </div>
   );
 };
