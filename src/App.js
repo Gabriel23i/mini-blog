524d1d61b2f0d9434140169a63763a13f8054d1a
Gabriel23i <ogabrielmb@gmail.com>
2022-10-16 11:29:55 -0300

    everything related to post creation

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 import './App.css';
 
@@ -34,38 +36,40 @@ function App() {
     return(<p>Carregando...</p>);
   };
 
-  return (
-    <div className='App'>
-      <AuthProvider value={{ user }}>
-        <BrowserRouter>
-          <Navbar />
-          <div className='container'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route
-                path='/login'
-                element={!user ? <Login /> : <Navigate to="/" />}
-              />
-              <Route
-                path='/register'
-                element={!user ? <Register /> : <Navigate to="/" />}
-              />
-              <Route
-                path='/posts/create'
-                element={user ? <CreatePost /> : <Navigate to='/login' />}
-              />
-              <Route
-                path='/dashboard'
-                element={user ? <Dashboard /> : <Navigate to='/login' />}
-              />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-      </AuthProvider>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <AuthProvider value={{ user }}>
+                <BrowserRouter>
+                    <Navbar />
+                    <div className='container'>
+                        <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/about' element={<About />} />
+                        <Route path='/search' element={<Search />} />
+                        <Route path='/post/:id' element={<Post />} />
+                        <Route
+                            path='/login'
+                            element={!user ? <Login /> : <Navigate to="/" />}
+                        />
+                        <Route
+                            path='/register'
+                            element={!user ? <Register /> : <Navigate to="/" />}
+                        />
+                        <Route
+                            path='/posts/create'
+                            element={user ? <CreatePost /> : <Navigate to='/login' />}
+                        />
+                        <Route
+                            path='/dashboard'
+                            element={user ? <Dashboard /> : <Navigate to='/login' />}
+                        />
+                        </Routes>
+                    </div>
+                    <Footer />
+                </BrowserRouter>
+            </AuthProvider>
+        </div>
+    );
 };
 
 export default App;
